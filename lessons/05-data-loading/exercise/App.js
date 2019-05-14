c87d9e198f6d85d9a0262bba4b89edd317d67988
ryan <ryanj.belke@gmail.com>
2019-05-14 15:29:07 -0500

    05 done

--- lessons/05-data-loading/exercise/App.js
+++ lessons/05-data-loading/exercise/App.js
@@ -1,12 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react"
 
-import { onAuthStateChanged } from 'app/utils'
-import LoggedIn from 'app/LoggedIn'
-import LoggedOut from 'app/LoggedOut'
+import { onAuthStateChanged } from "app/utils"
+import LoggedIn from "app/LoggedIn"
+import LoggedOut from "app/LoggedOut"
 
 export default function App() {
-  const auth = null
-  const authAttempted = false
+  const [auth, setAuth] = useState(null)
+  const [authAttempted, setAuthAttempted] = useState(false)
+
+  useEffect(() => {
+    onAuthStateChanged(auth => {
+      setAuthAttempted(true)
+      setAuth(auth)
+    })
+  }, [])
 
   if (!authAttempted) {
     return <p>Authenticating...</p>
@@ -18,3 +25,46 @@ export default function App() {
     </div>
   )
 }
+/*
+# Data Loading, Authentication
+
+We're going to be working in `App.js` to set up the login flow. The UI will render three states: loading, logged in, logged out.
+
+This app does client-side authentication with an auth listener called `onAuthStateChanged(auth => {})`. Whenever the user logs in, or out, this listener will callback with the auth state, including when its first created.
+
+```jsx
+onAuthStateChanged(auth => {
+  if (auth) {
+    // logged in
+  } else {
+    // logged out
+  }
+})
+```
+
+`onAuthStateChanged` returns an unsubscribe function to stop listening to auth state changes.
+
+```jsx
+const unsubscribe = onAuthStateChanged(cb)
+
+// later
+unsubscribe()
+```
+
+When displaying the logged in/out/loading UI there are two states of concern: if we have attempted to log in, and if we are logged in.
+
+The reason we want both states is so that we can render a "loading" indicator (or nothing) until we've at least attempted to log in, otherwise we'd end up flashing the logged out screen, which is kinda not awesome.
+
+## Instructions
+
+- add an effect that calls `onAuthStateChanged`
+- make sure it doesn't add a new handler every render
+- make sure to clean up the subscription
+
+Once that's working, refactor to a custom hook called `useAuth` that looks something like this when you use it.
+
+```
+const { auth, authAttemped } = useAuth()
+```
+
+*/
